c2a6b82ca404f763de30a15f475db826250c4fa1
BOMMANCHUAKHIL <102714542+BOMMANCHUAKHIL@users.noreply.github.com>
2025-11-02 10:08:07 +0530

    Update settings.py

--- cricpro/settings.py
+++ cricpro/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -261,3 +262,6 @@ SOCIALACCOUNT_PROVIDERS = {
         'AUTH_PARAMS': {'access_type': 'online'},
     }
 }
+# Add these static files settings
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
